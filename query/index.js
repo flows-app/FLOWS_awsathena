e649f17d5acf21f5451c5e75fda317f4fd0ba29c
Jens Walter <jens@apimeister.com>
2019-06-11 20:19:07 +0200

    include outputlocation

--- query/index.js
+++ query/index.js
@@ -8,11 +8,28 @@ exports.handler = async (event, context) => {
     QueryString: event.query,
     QueryExecutionContext: {
       Database: event.database
+    },
+    ResultConfiguration: {
+      EncryptionConfiguration: {
+        EncryptionOption: "SSE_S3"
+      },
+      OutputLocation: event.outputlocation
     }
   };
   let data = await ATHENA.startQueryExecution(params).promise();
   let execId = data.QueryExecutionId;
-  
+  let running = true;
+  while(running){
+    let params = {
+      QueryExecutionId: execId
+    };
+    let status = await ATHENA.getQueryExecution(params).promise();
+    if(status.QueryExecution.Status.State=="SUCCEEDED" ||
+      status.QueryExecution.Status.State=="FAILED" ||
+      status.QueryExecution.Status.State=="CANCELLED"){
+      running=false;
+    }
+  }
   let resultParams = {
     QueryExecutionId: execId,
   };
